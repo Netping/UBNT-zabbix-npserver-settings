08a988d23ea20033ebbfb7e528bf9c941df84d3c
lisyak <you@example.com>
2021-10-15 02:09:11 +0300

    1.1

--- config.sh
+++
@@ -1,4 +0,0 @@
-cd /tmp
-rsync -vr /tmp/isogit/iso/ /tmp/iso/netping/zabbix/
-rsync -vr /tmp/isogit/iso/config/prenps.sh /tmp/iso/netping/zabbix/config/
-rm -rf ./isogit
--- iso/config/prenps.sh
+++
@@ -1,5 +0,0 @@
-if ls /cdrom/netping/zabbix/ | grep NpServerSettings
-then
-cp -r /cdrom/netping/zabbix/NpServerSettings /target/usr/share/zabbix/modules/
-else echo "no NpServerSettings"
-fi
